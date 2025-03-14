3fea055ce9b209d36998c24c04333b345f380ec2
Ebdollah <l201165@lhr.nu.edu.pk>
2025-03-14 21:52:10 +0500

    fixed bug for build

---
+++ app/front-page.tsx
@@ -0,0 +1,9 @@
+import LandingPage from "@/app/(landing)/page/landing-page"
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <LandingPage />
+  </div>
+  );
+}
